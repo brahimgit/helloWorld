e1036e3f54b788a821df36e1414d0bc108e3e44f
brahim <salah_brahim2000@yahoo.fr>
2017-03-12 13:26:54 +0100

    PowerMock

--- firstGitProject/src/fr/fgp/controller/Controller.java
+++ firstGitProject/src/fr/fgp/controller/Controller.java
@@ -1,7 +1,21 @@
 package fr.fgp.controller;
 
+import java.util.ResourceBundle;
+
 public class Controller {
+	public Controller() {
+	}
 	public int somme(int a, int b){
 		return a+b;
+	}
+	public static String getLibille(){
+		String result = null;
+		ResourceBundle resourceBundle = ResourceBundle.getBundle("application");
+		resourceBundle.getString("socketServer.host");
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getLibille());
 	}
 }
--- firstGitProject/test/fr/fgp/controller/ControllerTest.java
+++ firstGitProject/test/fr/fgp/controller/ControllerTest.java
@@ -2,12 +2,12 @@ package fr.fgp.controller;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(org.powermock.modules.junit4.PowerMockRunner.class)
 public class ControllerTest {
 	@Test
 	public void test1() {
-		Assert.assertEquals(9, new Controller().somme(4, 5));
+		Assert.assertTrue(true);
 	}
-	
-	
 }
